d569c1963c043c4444d70ae0e3df890a597665ed
joemalebe <jtekniq@gmail.com>
2020-02-16 20:08:41 +0200

    create network repository

--- app/src/main/java/com/example/absolutesport/repository/NetworkRepository.kt
+++ app/src/main/java/com/example/absolutesport/repository/NetworkRepository.kt
@@ -1,5 +1,22 @@
 package com.example.absolutesport.repository
 
+import com.example.absolutesport.network.ApiService
+import com.example.absolutesport.network.Article
+import io.reactivex.Single
+import io.reactivex.schedulers.Schedulers
+import java.util.logging.Logger
+
 class NetworkRepository {
 
+    val apiService by lazy {
+        Logger.getAnonymousLogger().info("service initialising")
+        ApiService.create()
+    }
+
+    fun getTopHeadlines(): Single<List<Article>> {
+        return apiService.getTopHeadlines("za").subscribeOn(Schedulers.io()).map { response ->
+            Logger.getAnonymousLogger().info("map response with status ${response.status}")
+            response.articles
+        }
+    }
 }
