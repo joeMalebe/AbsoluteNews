cd8d364dfc362195e1f4965298f662d142fe963b
joemalebe <jtekniq@gmail.com>
2020-02-16 20:47:27 +0200

    create enums for service parameters

---
+++ app/src/main/java/com/example/absolutesport/data/Category.kt
@@ -0,0 +1,11 @@
+package com.example.absolutesport.data
+
+enum class Category(val value: String) {
+    ENTERTAINMENT(" entertainment "),
+    GENERAL(" general"),
+    HEALTH(" health"),
+    SCIENCE(" science"),
+    SPORTS(" sports"),
+    TECHNOLOGY(" technology"),
+    BUSINESS("business")
+}
