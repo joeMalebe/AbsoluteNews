fbcc90ae33f073fa14ed7cad1c770c9337bfd42c
joemalebe <jtekniq@gmail.com>
2020-02-16 20:11:34 +0200

    create service models

--- app/src/main/java/com/example/absolutesport/network/Article.kt
+++ app/src/main/java/com/example/absolutesport/network/Article.kt
@@ -6,13 +6,13 @@ import com.fasterxml.jackson.annotation.JsonProperty
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 data class Article(
-    @JsonProperty("author") val author: String,
-    @JsonProperty("title") val title: String,
-    @JsonProperty("description") val description: String,
-    @JsonProperty("url") val url: String,
-    @JsonProperty("urlToImage") val urlToImage: String,
-    @JsonProperty("publishedAt") val pulishedDate: String,
-    @JsonProperty("content") val content: String,
-    @JsonProperty("source") val source: Source
+    @JsonProperty("author") val author: String?,
+    @JsonProperty("title") val title: String?,
+    @JsonProperty("description") val description: String?,
+    @JsonProperty("url") val url: String?,
+    @JsonProperty("urlToImage") val urlToImage: String?,
+    @JsonProperty("publishedAt") val pulishedDate: String?,
+    @JsonProperty("content") val content: String?,
+    @JsonProperty("source") val source: Source?
 
 );
--- app/src/main/java/com/example/absolutesport/network/Source.kt
+++ app/src/main/java/com/example/absolutesport/network/Source.kt
@@ -4,4 +4,4 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties
 import com.fasterxml.jackson.annotation.JsonProperty
 
 @JsonIgnoreProperties(ignoreUnknown = true)
-data class Source(@JsonProperty("id")val id:String,@JsonProperty("name")val name:String)
+data class Source(@JsonProperty("id")val id:String?,@JsonProperty("name")val name:String?)
