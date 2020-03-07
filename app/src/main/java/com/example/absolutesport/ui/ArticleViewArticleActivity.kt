e2caced3259a3d1b23a766f84c50ca5f2bc7fb7a
joemalebe <jtekniq@gmail.com>
2020-03-07 22:02:14 +0200

    Add breakout to web after tapping readmore

--- app/src/main/java/com/example/absolutesport/ui/ArticleViewArticleActivity.kt
+++ app/src/main/java/com/example/absolutesport/ui/ArticleViewArticleActivity.kt
@@ -3,8 +3,14 @@ package com.example.absolutesport.ui
 import android.content.Context
 import android.content.Intent
 import android.graphics.BitmapFactory
+import android.net.Uri
 import android.os.Bundle
+import android.text.SpannableString
+import android.text.Spanned
+import android.text.method.LinkMovementMethod
+import android.text.style.ClickableSpan
 import android.util.Log
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.example.absolutesport.R
 import com.example.absolutesport.network.Article
@@ -29,6 +35,7 @@ class ArticleViewArticleActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_article_view)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
         val extras = intent.extras
         if (extras != null && extras.get(EXTRA_ARTICLE) != null) {
@@ -41,13 +48,29 @@ class ArticleViewArticleActivity : AppCompatActivity() {
     private fun displayScreen(article: Article) {
         text_article_title.setText(article.title)
 
-        text_content.setText(
+        val content =
             if (article.content != null) {
                 addReadMoreString(article.content)
             } else {
                 getString(R.string.read_more)
             }
+
+        val clickableSpan = object : ClickableSpan() {
+            override fun onClick(widget: View) {
+                val intent = Intent(Intent.ACTION_VIEW, Uri.parse(article.url))
+                startActivity(intent)
+            }
+        }
+
+        val spannableString = SpannableString(content)
+        spannableString.setSpan(
+            clickableSpan,
+            content.length - getString(R.string.read_more).length,
+            content.length,
+            Spanned.SPAN_EXCLUSIVE_EXCLUSIVE
         )
+        text_content.setText(spannableString)
+        text_content.movementMethod = LinkMovementMethod.getInstance()
         try {
             image_article_bilboard.setImageBitmap(BitmapFactory.decodeStream(openFileInput(article.hashCode().toString())))
 
@@ -58,6 +81,10 @@ class ArticleViewArticleActivity : AppCompatActivity() {
 
             )
         }
+    }
+
+    override fun onBackPressed() {
+        finish()
     }
 
     private fun addReadMoreString(content: String): String {
